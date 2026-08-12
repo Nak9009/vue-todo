3d97a647c49ed24500488a046bf7ae257e131b03
Ratanak Ieng <ratanak9009@gmail.com>
2026-08-12 10:45:58 +0700

    feat: add environment variable support for dynamic document title

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
+document.title = import.meta.env.VITE_APP_TITLE
 
 const app = createApp(App)
 
